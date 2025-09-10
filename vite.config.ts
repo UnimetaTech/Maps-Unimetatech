903858339fbe9faaf86dbf97fb553db942adf2b2
Redstone712 <sebastiancalchon@gmail>
2025-09-10 12:00:50 -0500

    feat: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: true,
+  },
+
   build: {
     rollupOptions: {
       output: {
